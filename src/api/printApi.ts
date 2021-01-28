6b89af9b46ffb090f24773930ef2f74eb5e968d2
daihy <dai875939260@163.com>
2021-01-28 10:50:27 +0800

    :recycle: Add Type define

--- src/api/printApi.ts
+++ src/api/printApi.ts
@@ -1,39 +1,65 @@
 import ClientApi, { ClientParam } from './clientApi';
 
+type FileType = 'pdf' | 'word' | 'image' | 'html';
+interface PrintWithTypeParam {
+    Url: string;
+    Fs: number;
+    FileType: FileType;
+    Printer?: string;
+}
+
+interface PrintParam {
+    Url: string;
+    Fs: number;
+    Printer?: string;
+}
+
+interface ClientPrintWithTypeParam extends ClientParam {
+    param: PrintWithTypeParam[];
+}
+
+interface ClientPrintParam extends ClientParam {
+    param: PrintParam[];
+}
+
 class PrintApi extends ClientApi {
-    print(param: ClientParam) {
+    /**
+     * 指定类型打印
+     * @param param {@see ClientPrintWithTypeParam}
+     */
+    print(param: ClientPrintWithTypeParam) {
         super.postSend('/api/print', param);
     }
 
     /**
      * 打印PDF
-     * @param param {@see ClientParam}
+     * @param param {@see ClientPrintParam}
      */
-    printPdf(param: ClientParam) {
+    printPdf(param: ClientPrintParam) {
         super.postSend('/api/print/pdf', param);
     }
 
     /**
      * 打印WORD
-     * @param param {@see ClientParam}
+     * @param param {@see ClientPrintParam}
      */
-    printWord(param: ClientParam) {
+    printWord(param: ClientPrintParam) {
         super.postSend('/api/print/word', param);
     }
 
     /**
      * 打印图片
-     * @param param {@see ClientParam}
+     * @param param {@see ClientPrintParam}
      */
-    printImage(param: ClientParam) {
+    printImage(param: ClientPrintParam) {
         super.postSend('/api/print/image', param);
     }
 
     /**
      * 打印图片
-     * @param param {@see ClientParam}
+     * @param param {@see ClientPrintParam}
      */
-    printHtml(param: ClientParam) {
+    printHtml(param: ClientPrintParam) {
         super.postSend('/api/print/html', param);
     }
 }
